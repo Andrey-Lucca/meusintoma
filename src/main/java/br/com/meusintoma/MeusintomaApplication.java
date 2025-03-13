b1a1bab4d00ac8446087dbb71a7ad17316070acf
Andrey Lucca <122308400+Andrey-Lucca@users.noreply.github.com>
2025-03-13 15:33:37 -0300

    Initial commit

---
+++ src/main/java/br/com/meusintoma/MeusintomaApplication.java
@@ -0,0 +1,13 @@
+package br.com.meusintoma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeusintomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeusintomaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meusintoma/MeusintomaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meusintoma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeusintomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
